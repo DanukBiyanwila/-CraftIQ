9b5191d3c03368d21db58e5c58ca57fb5676c8a4
Inupa Anjana <inupaanjana@gmail.com>
2025-05-15 15:41:06 +0530

    f
    footer

--- frontend/craftiq-client/src/components/Footer.jsx
+++ frontend/craftiq-client/src/components/Footer.jsx
@@ -18,9 +18,9 @@ function Footer() {
                       </div>
                       <div className="footer-tittle">
                         <div className="footer-pera">
-                          <p className="info1">Lorem ipsum dolor sit amet, nsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                          <p className="info2">198 West 21th Street, Suite 721 New York,NY 10010</p>
-                          <p className="info2">Phone: +95 (0) 123 456 789 Cell: +95 (0) 123 456 789</p>
+                          <p className="info1">Craft IQ is your smart hub for mastering creative skills—learn, create, and elevate your craft with expert guidance.</p>
+                          <p className="info2">No. 27, Creative Tower ,Galle Road, Colombo 03,Western Province, Sri Lanka</p>
+                          <p className="info2">Phone: +94 (0) 112 365 789<br></br> Cell: +94 (0) 715 456 789</p>
                         </div>
                       </div>
                     </div>
